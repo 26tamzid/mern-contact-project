5e6479180502f7f9bc5753bf40744bb6b17e40a6
26tamzid <earlymorning.bd@gmail.com>
2026-04-27 23:54:06 +0600

    for now this coding is done

--- contoler/contact.contoler.js
+++ contoler/contact.contoler.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 import contact from '../models/contact.model.js'
 
 
+// var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
+
 export const contactPage = async(req,res) => {
      const contacts =await contact.find();
     res.render('home',{contacts})
@@ -9,14 +11,25 @@ export const contactPage = async(req,res) => {
 
 
 export const singlePage = async(req,res)=>{
-    const contact1 = await contact.findOne({_id: req.params.id}) //findById(req.params.id) can use for make simple code
-    res.render('single-contact',{contact1})
+    var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
+    if(!paramId){
+        res.render('404',{messege: "invalid id"})
+    }
+    try{
+        const contact1 = await contact.findOne({_id: req.params.id}) //findById(req.params.id) can use for make simple code
+        if(!contact1)return res.render('404',{messege:'contact not found'})
+        res.render('single-contact',{contact1})
+    }catch(error){
+        res.render('500',{messege:'problem in code'})
+    }
+
     }
 
 export const addcontact = (req,res)=>{res.render('add-contact')}
 
 export const addcontactsubmit =async(req,res)=>{
-    const contact2 = await contact.insertOne({
+    try{
+            const contact2 = await contact.insertOne({
         first_name:req.body.first_name,
         last_name:req.body.last_name,
         email:req.body.email,
@@ -24,18 +37,52 @@ export const addcontactsubmit =async(req,res)=>{
         address:req.body.address
     })
     res.redirect("/")
+    }catch(error){
+        res.render('500',{messege:'error'})
+    }
+
 }
 
 export const updatecontact = async(req,res)=>{
-    const contact1 = await contact.findOne({_id: req.params.id}) 
-    res.render('update-contact',{contact1})
+    var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
+    if(!paramId){
+        res.render('404',{messege: "invalid id"})
+    }
+    try{
+        const contact1 = await contact.findOne({_id: req.params.id}) 
+        if(!contact1)return res.render('404',{messege:'contact not found'})
+        res.render('update-contact',{contact1})
+    }catch(error){
+        res.render('500',{messege:'problem in code'})
+    }   
 }
+
 export const updatecontactsubmit = async(req,res)=>{
-    await contact.findByIdAndUpdate(req.params.id,req.body)
-    res.redirect("/")
+    var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
+    if(!paramId){
+        res.render('404',{messege: "invalid id"})
+    }
+    try{
+       const contact1 = await contact.findByIdAndUpdate(req.params.id,req.body)
+        if(!contact1)return res.render('404',{messege:'contact not found'})
+        res.redirect("/")
+    }catch(error){
+        res.render('500',{messege:'problem in code'})
+    }
+    
 }
 
 export const deletecontact = async(req,res)=>{
-    await contact.findByIdAndDelete(req.params.id,req.body)
-     res.redirect("/")
+    var paramId = mongoose.Types.ObjectId.isValid(req.params.id)
+    if(!paramId){
+        res.render('404',{messege: "invalid id"})
+    }
+    try{
+        const contact1 = await contact.findByIdAndDelete(req.params.id,req.body)
+        if(!contact1)return res.render('404',{messege:'contact not found'})
+        res.redirect("/")
+    }catch(error){
+        res.render('500',{messege:'problem in code'})
+    }
+    
 }
